10be3a72b49105adfb6f2cc3f6a30a5468d8d1b1
zipthezap <51761473+zipthezap@users.noreply.github.com>
2023-11-28 19:13:16 -0500

    wip

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-// Fetch data from the endpoint
-fetch('http://localhost:3000/collab-app/pullRequests')
-  .then(response => {
-    // Check if the response is successful
+async function fetchData() {
+  try {
+    const response = await fetch('http://localhost:3000/collab-app/pullRequests');
+    
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    // Parse the response body as JSON
-    return response.json();
-  })
-  .then(data => {
-    // Process the retrieved data
-    console.log('Data received:', data);
-    // Handle or display the data as needed
-  })
-  .catch(error => {
-    // Handle errors that may occur during the fetch operation
+    
+    const data = await response.json();
+    return data;
+  } catch (error) {
     console.error('There was a problem with the fetch operation:', error);
-  });
+    return [];
+  }
+}
+
+async function populateTable() {
+  try {
+    const data = await fetchData();
+    console.log(data);
+    
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = '';
+
+    data.forEach(item => {
+      const row = document.createElement('tr');
+
+      Object.values(item).forEach(value => {
+        const cell = document.createElement('td');
+        cell.textContent = value;
+        row.appendChild(cell);
+      });
+
+      tableBody.appendChild(row);
+    });
+  } catch (error) {
+    console.error('Error while populating table:', error);
+  }
+}
+
+populateTable();
